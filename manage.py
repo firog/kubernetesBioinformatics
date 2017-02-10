2c8bb8d3880fbceac91a987e2fe5f622775e4ca9
firog <olofmarkstedt@gmail.com>
2017-02-10 13:42:55 +0100

    Register new users from command line

--- manage.py
+++ manage.py
@@ -1,9 +1,25 @@
 import os
 from app import create_app
 from flask_script import Manager
+from app import db
+from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+
+@manager.command
+def adduser(email, username, admin=False):
+	from getpass import getpass
+	password = getpass()
+	password2 = getpass(prompt='Confirm: ')
+	if password != password2:
+		import sys
+		sys.exit('Error: passwords do not match.')
+	db.create_all()
+	user = User(email=email, username=username, password=password, is_admin=admin)
+	db.session.add(user)
+	db.session.commit()
+	print('User {0} was registered successfully.'.format(username))
 
 if __name__ == '__main__':
 	manager.run()
