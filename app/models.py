92ac6fdde76145b81f3e2a8cb4ebe41a07046ff5
firog <olofmarkstedt@gmail.com>
2017-02-10 15:09:02 +0100

    Added login form

--- app/models.py
+++ app/models.py
@@ -18,14 +18,14 @@ class User(UserMixin, db.Model):
 
 	@property
 	def password(self):
-		raise AttributeError('password is not a readable attribute')
+		raise AttributeError('Password is not a readable attribute')
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
 
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 @login_manager.user_loader
 def load_user(user_id):
