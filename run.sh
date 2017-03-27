a7794012100029ccabbaa74356282840e6bca3f5
firog <olofmarkstedt@gmail.com>
2017-03-27 14:11:39 +0200

    updated kubernetes templates

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-celery multi start worker1 -A celery_worker.celery --broker=amqp://guest:guest@$RABBITMQ_SERVICE_SERVICE_HOST:5672// #-pidfile="$HOME/run/celery/pid" --logfile="$HOME/log/celery/log"
+sleep 10
+celery multi start worker1 -A celery_worker.celery --broker=amqp://guest:guest@$RABBITMQ_SERVICE_SERVICE_HOST:5672// --logfile="$HOME/%n%I.log"
 python manage.py runserver --host 0.0.0.0 --port 5000
 #mkidr -p $HOME/run/celery
 
